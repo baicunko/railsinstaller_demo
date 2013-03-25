46a568e58b5fe19a72c31094bfde827a22b031cf
Rafael Lecaros <rlecaro2@uc.cl>
2013-03-25 15:53:24 -0400

    Only one marker that moves around

--- app/assets/javascripts/google.js
+++ app/assets/javascripts/google.js
@@ -1,3 +1,21 @@
+var map;
+var marker;
+
+
+
+function createMarker(event){
+  //alert("Latitude: "+event.latLng.lat()+" "+", longitude: "+event.latLng.lng());
+  //addMarker(event.latLng);
+  if(marker){marker.setPosition(event.latLng);}
+  else{
+    marker = new google.maps.Marker({
+      position: event.latLng,
+      map: map
+    });
+  }
+}
+
+
 
 function initialize() {
   var mapOptions = {
@@ -5,12 +23,10 @@ function initialize() {
     zoom: 8,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-  var map = new google.maps.Map(document.getElementById("map-canvas"),
+  map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
-  
-  google.maps.event.addListener(map, 'click', function( event ){
-  alert( "Latitude: "+event.latLng.lat()+" "+", longitude: "+event.latLng.lng() ); 
-});
+
+  google.maps.event.addListener(map, 'click', function(event){createMarker(event);});
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
